b572f02a9231f29672fef7cf27326c210526c8ff
s13012 <s13012@std.it-college.ac.jp>
2015-06-15 19:41:06 +0900

    ブロックの種類追加

--- test.py
+++ test.py
@@ -1,18 +1,54 @@
 import pyglet
+import random
 
 game_window = pyglet.window.Window(width=640, height=480)
 
 block = pyglet.resource.image("block.png")
 BLOCKS = [
-    [1, 1],
-    [1, 1]
+    [
+        [1, 1],
+        [1, 1]
+    ],
+    [
+        [1, 1],
+        [0, 1],
+        [0, 1]
+    ],
+    [
+        [1, 1],
+        [1, 0],
+        [1, 0]
+    ],
+    [
+        [1, 0],
+        [1, 1],
+        [1, 0]
+    ],
+    [
+        [1, 0],
+        [1, 1],
+        [0, 1]
+    ],
+    [
+        [0, 1],
+        [1, 1],
+        [1, 0]
+    ],
+    [
+        [1],
+        [1],
+        [1],
+        [1]
+    ]
 ]
 
 @game_window.event
 def on_draw():
-    for y in range(len(BLOCKS)):
-        for x in range(len(BLOCKS[y])):
-            if BLOCKS[y][x] == 1:
+    game_window.clear()
+    tmp = random.choice(BLOCKS)
+    for y in range(len(tmp)):
+        for x in range(len(tmp[y])):
+            if tmp[y][x] == 1:
                 block.blit(x * 16, y * 16, 0, width=16, height=16)
 
 pyglet.app.run()
